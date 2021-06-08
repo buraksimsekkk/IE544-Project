3ea0a415a7c097b70bf2e8ac8bc4346c36de3b1e
buraksimsekkk <buraksimsek50@gmail.com>
2021-06-08 23:14:54 +0300

    update

--- IE544_PROJECT_Q1.r
+++ IE544_PROJECT_Q1.r
@@ -17,7 +17,7 @@ colnames(data)[2] = "year"
 colnames(data)[3] = "month"
 colnames(data)[4] = "day"
 
-###a
+###a-) Number of Products, number of sellers (overall and by day)
 length(unique(data$pid))
 unique(data$pid)
 length(unique(data$sid))
@@ -37,18 +37,18 @@ data = data[,-c(3,4)]
 #data
 data = add_column(data, month_day = month_day, .after = 1)
 #data
-seller_count_byday = data %>% group_by(month_day) %>% summarise(seller_count_byday = length(unique(sid)))
 product_count_byday = data %>% group_by(month_day) %>% summarise(product_count_byday = length(unique(pid)))
+seller_count_byday = data %>% group_by(month_day) %>% summarise(seller_count_byday = length(unique(sid)))
 
 barplot(seller_count_byday$seller_count_byday,names.arg=seller_count_byday$month_day,xlab="Date",ylab="No_of_sellers",col="green")
 barplot(product_count_byday$product_count_byday,names.arg=product_count_byday$month_day,xlab="Date",ylab="No_of_products",col="red")
 
-##b
+##b-) Average, max, min price, buy-box price, shipping cost by product
 
 avg = data.frame( data %>% group_by(pid,month_day) %>% summarise(average = mean(price)))
 max = data.frame(data %>% group_by(pid,month_day) %>% summarise(max = max(price)))
 min = data.frame(data %>% group_by(pid,month_day) %>% summarise(min = min(price)))
-df=data%>% filter(data$bbox=="success")
+df = data%>% filter(data$bbox=="success")
 bbox_price = data.frame(df %>% group_by(pid,month_day) %>% summarise(bbox_price = mean(bbox_price)))
 
 for (i in c(1:9)){
@@ -76,27 +76,53 @@ for (i in c(1:9)){
 }
 
 
+###############
+#  shipping cost by product eklenmeli
+##############
 
 
-##c
+
+
+##c-) Seller ratings, positive feedbacks and counts, product ratings and counts by product
 data %>% group_by(pid) %>% summarise(average_sid_rating = mean(sid_rating))
 data %>% group_by(pid) %>% summarise(average_sid_pos_fb = mean(sid_pos_fb))
 data %>% group_by(pid) %>% summarise(average_sid_rating_cnt = mean(sid_rating_cnt))
 data %>% group_by(pid) %>% summarise(average_pid_rating = mean(pid_rating))
 data %>% group_by(pid) %>% summarise(average_pid_rating_cnt = mean(pid_rating_cnt))
 
-##d
+##d-) Percentage of buy-box successes when Amazon is the seller against every other seller (total and by product and week)
 ##by product
-data %>% filter(sid=="amazon", bbox=="success") %>% group_by(pid) %>% summarise(No_of_Amazon_Win = length((sid)))
-data %>% filter(sid=="amazon")%>% group_by(pid) %>% summarise(is_Amazon_seller = length((sid)))
+a = data %>% filter(sid=="amazon", box=="success") %>% group_by(pid) %>% summarise(No_of_Amazon_Win = length((sid))) 
+a = a %>% add_row(pid="B00MVVI1FC" , No_of_Amazon_Win=0, .before = 5) 
+
+b = data %>% filter(sid=="amazon")%>% group_by(pid) %>% summarise(is_Amazon_seller = length((sid)))
+
+winper = a[,2]/b[,2] 
+winper = winper   %>% add_column(a[,1], .before = 1) %>% add_row(pid="B00AMFLZLG" , No_of_Amazon_Win=NA, .before = 3)
+
 
 ##total
-a=data %>% filter(sid=="amazon", bbox=="success") %>% group_by(pid) %>% summarise(No_of_Amazon_Win = length((sid)))
+a = data %>% filter(sid=="amazon", box=="success") %>% group_by(pid) %>% summarise(No_of_Amazon_Win = length((sid)))
 colSums(a[,2])
-b=data %>% filter(sid=="amazon")%>% group_by(pid) %>% summarise(is_Amazon_seller = length((sid)))
+b = data %>% filter(sid=="amazon")%>% group_by(pid) %>% summarise(is_Amazon_seller = length((sid)))
 colSums(b[,2])
 
-##e
+winpertotal = colSums(a[,2])/colSums(b[,2])
+
+winper = winper %>% add_row(pid="total", No_of_Amazon_Win=winpertotal)
+
+###############
+#Weekly ratio eklenmeli
+##############
+
+
+##e-) Prime, FBA, Page and Rank information by seller and by product
+##is_Prime
+data %>% filter(bbox=="success", is_prime=="yes") %>% group_by(pid) %>% summarise(two_condition = length((is_prime)))
+data %>% filter(bbox=="success", is_prime=="yes") %>% group_by(sid) %>% summarise(two_condition = length((is_prime)))
+##is_fba
+data %>% filter(bbox=="success", is_fba=="yes") %>% group_by(pid) %>% summarise(two_condition = length((is_fba)))
+data %>% filter(bbox=="success", is_fba=="yes") %>% group_by(sid) %>% summarise(two_condition = length((is_fba)))
 ##Rank
 data %>% filter(bbox=="success") %>% group_by(rank) %>% summarise(no_of_bbox_success = length((rank)))
 data %>% filter(bbox=="success") %>% group_by(pid,rank) %>% summarise(no_of_bbox_success = length((rank)))
@@ -106,14 +132,8 @@ data %>% filter(bbox=="success") %>% group_by(page) %>% summarise(no_of_bbox_suc
 data %>% filter(bbox=="success") %>% group_by(pid,page) %>% summarise(no_of_bbox_success = length((page)))
 data %>% filter(bbox=="success") %>% group_by(sid,page) %>% summarise(no_of_bbox_success = length((page)))
 
-##is_Prime
-data %>% filter(bbox=="success", is_prime=="yes") %>% group_by(pid) %>% summarise(two_condition = length((is_prime)))
-data %>% filter(bbox=="success", is_prime=="yes") %>% group_by(sid) %>% summarise(two_condition = length((is_prime)))
-##is_fba
-data %>% filter(bbox=="success", is_fba=="yes") %>% group_by(pid) %>% summarise(two_condition = length((is_fba)))
-data %>% filter(bbox=="success", is_fba=="yes") %>% group_by(sid) %>% summarise(two_condition = length((is_fba)))
 
-##g
+##g-) What are the scales on which the data in each column is measured on?
 str(data)
 summary(data)
 
@@ -125,10 +145,10 @@ boxplot(data$shipping)
 boxplot(data$pid_rating)
 boxplot(data$pid_rating_cnt)
 
-a=data %>% filter(bbox=="success") %>% group_by(pid) %>% summarise(result = mean((sid_rating_cnt)))
-b=data %>% group_by(pid) %>% summarise(average_sid_rating_cnt = mean(sid_rating_cnt))
-a
-b
+aa=data %>% filter(bbox=="success") %>% group_by(pid) %>% summarise(result = mean((sid_rating_cnt)))
+bb=data %>% group_by(pid) %>% summarise(average_sid_rating_cnt = mean(sid_rating_cnt))
+aa
+bb
 
 
 data %>% filter(bbox=="failure") %>% group_by(page) %>% summarise(no_of_bbox_success = length((page)))
